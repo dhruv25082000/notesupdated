aebb20c038d0ea8faf57b9aaf2688cb2c647833f
dhruv <dhruvraimi6@gmail.com>
2022-08-23 02:59:20 +0530

    final changes

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
   try {
-    const db = await mongoose.connect(MONGODB_URI);
+    const db = await mongoose.connect("mongodb+srv://imabhi:loveanjali@cluster0.2uce9kq.mongodb.net/?retryWrites=true&w=majority");
     console.log("Mongodb is connected to", db.connection.host);
   } catch (error) {
     console.error(error);
